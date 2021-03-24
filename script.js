685e5f54acc1c71f588e14a36ca007706bbc8853
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-24 16:05:33 +0000

    Fixed variables before document.getElementById under Function.

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.getElementById('button1').addEventListener('click', pricing)
 
 // Finally, I have to program my function to carry out statements. When the statements are followed, an appropriate response will display.
 function pricing () {
-  document.getElementById('text1').value
-  document.getElementById('text2').value
+  age = document.getElementById('text1').value
+  day = document.getElementById('text2').value
   age = parseInt(age)
-  if ((age >= 12 && age <= 21) && (day === Wednesday || day === Thursday)) {
+  if ((age >= 12 && age <= 21) && (day == 'Wednesday' || day == 'Thursday')) {
     document.getElementById('results').innerHTML = 'You are eligible for the student pricing!'
   } else {
     document.getElementById('results').innerHTML = 'You are not eligible for student pricing.'
